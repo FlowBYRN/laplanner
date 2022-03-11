6ef6ea43dc3531f9960a890e0e4bc0a4d71021e9
FlowBYRN <weidnerflo@gmail.com>
2022-03-11 11:58:24 +0100

    Added Postgres Project

---
+++ Trainingsplanner.Postgres/Data/ApplicationDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Trainingsplanner.Postgres.Models;
+using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Duende.IdentityServer.EntityFramework.Options;
+
+namespace Trainingsplanner.Postgres.Data
+{
+    public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext(
+            DbContextOptions options,
+            IOptions<OperationalStoreOptions> operationalStoreOptions) : base(options, operationalStoreOptions)
+        {
+        }
+    }
+}
